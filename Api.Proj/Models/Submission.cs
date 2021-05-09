0e22c13f007298d6a4e9d6eb85e412ecee62fdd0
lds <lifeslong@gmail.com>
2021-05-10 00:25:45 +0200

    Enable LAzy loading by virtualising

--- Api.Proj/Models/Submission.cs
+++ Api.Proj/Models/Submission.cs
@@ -6,8 +6,8 @@ namespace WordFreqApi.Models
         public long SubmissionId { get; set;}
         public string Submitter { get; set;}
         public string Content {get; set;}
-        public List<HighFrequencyWord> ContentFreq {get; set;}
+        public virtual List<HighFrequencyWord> ContentFreq {get; set;}
         public string Comment {get; set;}
-        public Source Source {get; set;}
+        public virtual Source Source {get; set;}
     }
 }
